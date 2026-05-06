f622ea8cae7a4cb09861533835a0247b524da226
sameeksha876 <sameekshakhurpe08@gmail.com>
2026-05-06 15:54:06 +0530

    Sameeksha Added UC9 weight measurement support

---
+++ src/main/com/quantitymeasurement/WeightUnit.java
@@ -0,0 +1,26 @@
+package com.quantitymeasurement;
+
+public enum WeightUnit {
+
+    KILOGRAM(1000.0),
+    GRAM(1.0),
+    POUND(453.592);
+
+    private final double conversionFactor;
+
+    WeightUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+}
