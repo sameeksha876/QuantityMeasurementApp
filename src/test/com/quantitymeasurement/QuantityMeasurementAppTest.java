03a748e6df0036cdadab0843408e820063df83cb
sameeksha876 <sameekshakhurpe08@gmail.com>
2026-05-06 14:40:37 +0530

    Sameeksha Added UC2 feet and inches equality

--- src/test/com/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/com/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
@@ -40,5 +41,39 @@ public class QuantityMeasurementAppTest {
         Feet feet = new Feet(1.0);
 
         assertEquals(feet, feet);
+    }
+
+    @Test
+    void testInchesEquality_SameValue() {
+
+        Inches inch1 = new Inches(1.0);
+        Inches inch2 = new Inches(1.0);
+
+        assertEquals(inch1, inch2);
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+
+        Inches inch1 = new Inches(1.0);
+        Inches inch2 = new Inches(2.0);
+
+        assertNotEquals(inch1, inch2);
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+
+        Inches inch = new Inches(1.0);
+
+        assertNotEquals(null, inch);
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+
+        Inches inch = new Inches(1.0);
+
+        assertEquals(inch, inch);
     }
 }
