367b8f50a36a4839245c7939539779dfe7faadb8
sameeksha876 <sameekshakhurpe08@gmail.com>
2026-05-06 15:37:47 +0530

    Sameeksha Added UC8 standalone length unit

---
+++ src/main/com/quantitymeasurement/LengthUnit.java
@@ -0,0 +1,27 @@
+package com.quantitymeasurement;
+
+public enum LengthUnit {
+
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
+
+    private final double conversionFactor;
+
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+}
